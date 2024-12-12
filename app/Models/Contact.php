6f5c834341cdac004f65befbf820a7c1fa9a2bf9
ivan-akos <ivan.akos.asd@gmail.com>
2024-12-13 00:23:47 +0100

    Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
-    //
+    use HasUuids, SoftDeletes;
+    
+    protected $fillable = [
+        'user_id',
+        'contact_id',
+        'accepted'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function contact()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
