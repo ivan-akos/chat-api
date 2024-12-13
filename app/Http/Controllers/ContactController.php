a0ed61402091127452f25de4b369106bae6e3a50
ivan-akos <ivan.akos.asd@gmail.com>
2024-12-13 03:45:55 +0100

    ContactController acceptRequest

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    //TODO: Proper validation, denyRequest, showPending
+
+
     public function sendRequest($contact_id)
     {
         $user = auth('sanctum')->user();
@@ -17,5 +21,29 @@ class ContactController extends Controller
         ]);
 
         return response()->json(['message' => 'Contact request sent!', 'contact' => $contact]);
+    }
+
+    public function acceptRequest($contact_id)
+    {
+        $user = auth('sanctum')->user();
+
+        if(!User::findOrFail($contact_id)){
+            return response()->json([
+                'message' => 'User not found'
+            ]);
+        }
+
+        //           user_id and contact_id are flipped \/ here
+        $contactRequest = Contact::where('contact_id', $user->id)
+                                ->where('user_id', $contact_id)
+                                ->where('accepted', false)
+                                ->firstOrFail();
+
+        $contactRequest->update(['accepted' => true]);
+
+        return response()->json([
+            'message' => 'Contact request accepted!',
+            'contact' => $contactRequest,
+        ]);
     }
 }
