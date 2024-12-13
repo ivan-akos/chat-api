33c3ede2a5f22a90e593bfb53bdda22917f6730b
ivan-akos <ivan.akos.asd@gmail.com>
2024-12-13 10:44:51 +0100

    resend verification email

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -11,22 +11,21 @@ class EmailVerificationController extends Controller
 {
     public function verify(Request $request)
     {
-        
         $user = User::findOrFail($request->id);
 
         if (!$user->hasVerifiedEmail()) {
             $user->markEmailAsVerified();
         }
-        // if ($user->markEmailAsVerified()) {
-        //     event(new Verified($user));
-        // }
+
         return response([
             'message' => 'Email verified successfully'
         ]);
     }
 
-    public function resend()
+    public function resend(Request $request)
     {
-        //TODO
+        $request->user()->sendEmailVerificationNotification();
+        
+        return response()->json(['message' => 'Verification link sent!']);
     }
 }
