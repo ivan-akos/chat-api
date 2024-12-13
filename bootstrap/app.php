467b2e3a1bf37079cc589866614eae0c0d8c3b7e
ivan-akos <ivan.akos.asd@gmail.com>
2024-12-13 08:48:36 +0100

    added UserActivity middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\UserActivity;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(
+            append: [ 
+                UserActivity::class
+            ]
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
