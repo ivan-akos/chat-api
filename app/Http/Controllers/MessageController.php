ddd9a2cfa9f0cfe26a5ad39fe07b1d3c633c4113
ivan-akos <ivan.akos.asd@gmail.com>
2024-12-13 08:44:41 +0100

    MessageController retrieveFrom

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function retrieveFrom($contact_id){
+        $user = auth('sanctum')->user();
+
+        $messages = $user->receivedMessages()
+            ->where('sender_id', $contact_id)
+            ->get();
+
+        $user->receivedMessages()
+            ->where('sender_id', $contact_id)
+            ->where('is_read', false)           // update unread messages
+            ->update(['is_read' => true]);
+    
+        return response()->json([
+            'message' => 'Messages retrieved', 
+            'data' => $messages
+        ]);
+    }
 }
